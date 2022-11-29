be9ea89bc42a28b164a47ee3c5dab37bd540a2cc
Aleksandr Serenko <fafnur@yandex.ru>
2022-11-29 17:10:40 +0700

    [scripts] Make release in single file

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,41 +1,109 @@
-import { writeFileSync } from 'fs';
+import { writeFileSync, readFileSync } from 'fs';
 import { join } from 'path';
+import { output } from '@nrwl/workspace';
+import { execSync } from 'child_process';
 
-const publishLibs = ['libs/core'];
+/**
+ * Read data
+ *
+ * @param path Path to file
+ */
+function read(path: string): { version: string } {
+  return JSON.parse(readFileSync(path).toString());
+}
 
-const fileName = `${__dirname}/../package.json`;
-const file: { version: string } = require(fileName);
+/**
+ * Return a new version for NX workspace
+ */
+function getVersion(): string {
+  const config = read('package.json');
 
-const [major, minor, patch] = file.version.split('.').map((part) => Number(part));
-const [, , releaseVersion] = process.argv;
+  const [major, minor, patch] = config.version.split('.').map((part) => Number(part));
+  const [, , releaseVersion] = process.argv;
 
-let version: string;
+  let version: string;
 
-switch (releaseVersion) {
-  case 'major': {
-    version = `${major + 1}.0.0`;
-    break;
+  switch (releaseVersion) {
+    case 'major': {
+      version = `${major + 1}.0.0`;
+      break;
+    }
+    case 'minor': {
+      version = `${major}.${minor + 1}.0`;
+      break;
+    }
+    default: {
+      version = `${major}.${minor}.${patch + 1}`;
+      break;
+    }
   }
-  case 'minor': {
-    version = `${major}.${minor + 1}.0`;
-    break;
-  }
-  default: {
-    version = `${major}.${minor}.${patch + 1}`;
-    break;
+
+  return version;
+}
+
+/**
+ * Write version on package.json
+ *
+ * @param path Path to package.json
+ * @param version New version
+ */
+function write(path: string, version: string): void {
+  const config = read(path);
+  config.version = version;
+  writeFileSync(path, JSON.stringify(config, null, 2));
+}
+
+/**
+ * Update versions
+ */
+function updateVersion(version: string, libs: string[]): void {
+  ['package.json', ...libs.map((lib) => join(lib, 'package.json'))].forEach((path) => write(path, version));
+}
+
+function createGitRelease(version: string): void {
+  const branch = `release/v${version}`;
+  execSync(`git checkout -b ${branch}`);
+  execSync('git add .');
+  execSync(`git commit -m "version up v${version}"`);
+  execSync('git checkout develop');
+  execSync(`git merge ${branch}`);
+  execSync(`git branch --delete ${branch}`);
+  execSync(`git checkout main`);
+  execSync(`git rebase develop`);
+  execSync(`git push -u origin main develop`);
+}
+
+/**
+ * Publish libs
+ * @param libs
+ */
+function publishNpm(libs: string[]): void {
+  for (const lib of libs) {
+    execSync(`cd ${__dirname}`);
+    execSync(`nx build ${lib.replace(new RegExp('/', 'g'), '-')}`);
+    execSync(`cd ${join(__dirname, '../dist', lib)}`);
+    execSync('npm publish --access public');
   }
 }
 
-for (const lib of publishLibs) {
-  const path = join(`${__dirname}/../`, lib, 'package.json');
+function release(libs: string[]): void {
+  try {
+    output.log({ title: 'Starting a new release.' });
 
-  const fileLib: { version: string } = require(path);
-  fileLib.version = version;
+    const version = getVersion();
+    updateVersion(version, libs);
+    output.log({ title: 'New version was updated on packages.' });
 
-  writeFileSync(path, JSON.stringify(fileLib, null, 2));
+    createGitRelease(version);
+    output.log({ title: 'Release was merged on main branch.' });
+
+    publishNpm(libs);
+    output.log({ title: 'Release was published on npm.' });
+
+    output.log({ title: 'New version creation completed successfully.' });
+  } catch (e) {
+    output.error({ title: 'Creation a new release did not complete successfully' });
+  }
 }
 
-file.version = version;
-writeFileSync(fileName, JSON.stringify(file, null, 2));
-
-console.log(file.version);
+release(['libs/core']);
