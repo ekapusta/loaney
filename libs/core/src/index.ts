14ec042da72b7b0aeb590da71905338fa97a1f5e
Aleksandr Serenko <fafnur@yandex.ru>
2022-11-30 08:45:45 +0700

    [core] Adding export libs

--- libs/core/src/index.ts
+++ libs/core/src/index.ts
@@ -1,2 +1,7 @@
 export * from './lib/environments/environment.service';
+export * from './lib/icons/icon.service';
+export * from './lib/operators/extract.operator';
+export * from './lib/operators/not-null-or-undefined.operator';
 export * from './lib/platform/platform.service';
+export * from './lib/types/form.type';
+export * from './lib/types/nullable.type';
